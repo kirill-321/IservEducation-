c6dde50159381049cd27cae35882e250f78ce0de
kirill-321 <kirills2010s@gmail.com>
2026-01-17 19:00:53 +0300

    Добавил 16 урок

---
+++ Python/lesson16/main.py
@@ -0,0 +1,4 @@
+from app import *
+
+myapp = App()
+myapp.start()
---
+++ Python/lesson16/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, name, phone, password):
+        self.__name = name
+        self.__phone = phone
+        self.__password = password
+        self.__account = 0
